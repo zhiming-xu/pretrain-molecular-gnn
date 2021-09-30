412a105e2c10a2e1e1345dbdd757039032389684
Zhiming Xu <zhimng.xu@gmail.com>
2021-09-29 22:04:39 -0400

    add test

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import torch as th
+from torch.nn.modules import loss
 from torch.optim import SGD, Adam
 from torch.utils.tensorboard import SummaryWriter
 import os.path
@@ -7,7 +8,7 @@ from argparse import ArgumentParser
 from datetime import datetime
 from tqdm import tqdm
 
-from nn import SSLMolecule, DistanceExpansion
+from nn import PropertyPrediction, SSLMolecule, DistanceExpansion
 from data_utils import *
 
 
@@ -16,6 +17,7 @@ parser.add_argument('-data_dir', type=str, default='data/')
 parser.add_argument('-dataset', type=str, default='qm7')
 parser.add_argument('-num_atom_types', type=int, default=5)
 parser.add_argument('-atom_emb_size', type=int, default=32)
+parser.add_argument('-atom_pos_size', type=int, default=3)
 parser.add_argument('-cutoff', type=float, default=5.)
 parser.add_argument('-dist_exp_size', type=int, default=16)
 parser.add_argument('-hidden_size', type=int, default=32)
@@ -24,19 +26,36 @@ parser.add_argument('-gaussian_size', type=int, default=32)
 parser.add_argument('-num_type_layers', type=int, default=3)
 parser.add_argument('-num_pos_layers', type=int, default=3)
 parser.add_argument('-num_vae_layers', type=int, default=3)
+parser.add_argument('-num_pred_layers', type=int, default=3)
 parser.add_argument('-weight_l1', type=float, default=1)
 parser.add_argument('-weight_l2', type=float, default=0.1)
 parser.add_argument('-cuda', action='store_true')
+parser.add_argument('-pretrain', action='store_true')
+parser.add_argument('-test', action='store_true')
 parser.add_argument('-lr', type=float, default=1e-3)
-parser.add_argument('-num_epochs', type=int, default=100)
+parser.add_argument('-pretrain_epochs', type=int, default=100)
+parser.add_argument('-pred_epochs', type=int, default=200)
 parser.add_argument('-ckpt_step', type=int, default=5)
+parser.add_argument('-ckpt_file', type=str)
 
 
 def main():
     args = parser.parse_args()
+    args.running_id = '%s_%s' % (args.dataset, datetime.strftime(datetime.now(), '%Y-%m-%d_%H%M'))
+    # create tensorboard summary writer
+    sw = SummaryWriter(f'logs/{args.running_id}')
+
+    if args.pretrain:
+        pretrain(args, sw)
+    if args.test:
+        pred(args, sw)
+
+
+def pretrain(args, sw):
+    # save arguments
     datafile = os.path.join(args.data_dir, args.dataset)
 
-    Rs, Zs, Ds = load_qm7(datafile)
+    Rs, Zs, Ds, _ = load_qm7_dataset(datafile)
 
     model = SSLMolecule(num_atom_types=args.num_atom_types,
         atom_emb_size=args.atom_emb_size, dist_exp_size=args.dist_exp_size,
@@ -49,19 +68,13 @@ def main():
         model = model.cuda()
 
     optimizer = SGD(model.parameters(), lr=args.lr)
-    running_id = '%s_%s' % (args.dataset, datetime.strftime(datetime.now(), '%Y-%m-%d_%H%M'))
-    # create tensorboard summary writer
-    sw = SummaryWriter(f'logs/{running_id}')
-    # save arguments
-    with open(f'logs/{running_id}/args.json') as f:
-        json.dump(vars(args), f)
 
     dist_exp = DistanceExpansion(repeat=args.dist_exp_size)
     cutoff = args.cutoff
 
     weight_l1, weight_l2 = args.weight_l1, args.weight_l2
 
-    for epoch in tqdm(range(args.num_epochs)):
+    for epoch in tqdm(range(args.pretrain_epochs)):
         loss_atom_preds, loss_pos_preds, loss_vaes, losses = [], [], [], []
         for R, Z, D in zip(Rs, Zs, Ds):
             model.zero_grad()
@@ -73,8 +86,9 @@ def main():
             D_exp = dist_exp(D)
  
             loss_atom_pred, loss_pos_pred, loss_vae = model(R, A, D_exp, Z)
-            loss = weight_l1 * (loss_atom_pred + loss_pos_pred) + weight_l2 * loss_vae
-            
+            loss = weight_l1 * (loss_atom_pred + loss_pos_pred/5) + \
+                   weight_l2 * (epoch//20) * loss_vae
+             # temporary workout to gradually increase kl loss weight
             if loss_atom_pred > 10 or loss_pos_pred > 120:
                 continue
 
@@ -93,7 +107,56 @@ def main():
         sw.add_scalar('Loss/Total', sum(losses)/total, epoch)
 
         if (epoch+1) % args.ckpt_step == 0:
-            th.save(model.state_dict(), 'ckpt/epoch_%d.th' % epoch)
+            th.save(model.state_dict(), f'logs/{args.running_id}/epoch_%d.th' % epoch)
+
+
+def pred(args, sw):
+    with open(f'logs/{args.running_id}/args.json', 'w') as f:
+        json.dump(vars(args), f)
+    datafile = os.path.join(args.data_dir, args.dataset)
+
+    Rs, Zs, _, Ts = load_qm7_dataset(datafile)
+
+    pretrain_model = SSLMolecule(num_atom_types=args.num_atom_types,
+        atom_emb_size=args.atom_emb_size, dist_exp_size=args.dist_exp_size,
+        hidden_size=args.hidden_size, pos_size=args.pos_size,
+        gaussian_size=args.gaussian_size, num_type_layers=args.num_type_layers,
+        num_pos_layers=args.num_pos_layers, num_vae_layers=args.num_vae_layers
+    )
+
+    pretrain_model.load_state_dict(th.load(args.ckpt_file))
+
+    input_size = args.atom_emb_size + args.atom_pos_size
+    pred_model = PropertyPrediction(input_size, args.hidden_size, args.num_pred_layers)
+
+    optimizer1 = SGD(pretrain_model.parameters(), lr=1e-6)
+    optimizer2 = SGD(pred_model.parameters(), lr=1e-3)
+
+    for epoch in tqdm(range(args.pred_epochs)):
+        pred_losses, test_losses = [], []
+        cnt = 5000
+        for R, Z, T in zip(Rs, Zs, Ts):
+            cnt -= 1
+            if cnt >= 0:
+                pretrain_model.zero_grad()
+                pred_model.zero_grad()
+                R, Z, T = th.FloatTensor(R), th.LongTensor(Z), th.FloatTensor(T)
+                h = pretrain_model.encode(R, Z)
+                loss = pred_model(h, T)
+                loss.backward()
+                pred_losses.append(loss)
+                optimizer1.step()
+                optimizer2.step()
+            else:
+                with th.no_grad():
+                    R, Z, T = th.FloatTensor(R), th.LongTensor(Z), th.FloatTensor(T)
+                    h = pretrain_model.encode(R, Z)
+                    loss = pred_model(h, T)
+                    test_losses.append(loss)
+        
+        total = len(Ts)
+        sw.add_scalar('Loss/Pred', sum(pred_losses)/cnt, epoch)
+        sw.add_scalar('Loss/Test', sum(test_losses)/(total-cnt), epoch)
 
 
 if __name__ == '__main__':
