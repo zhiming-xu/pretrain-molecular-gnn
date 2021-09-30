2965a79417f87d681e8f1c6e285674c8383f34be
Zhiming Xu <zhimng.xu@gmail.com>
2021-09-29 22:05:02 -0400

    fix dataloader return value

--- data_utils.py
+++ data_utils.py
@@ -5,12 +5,16 @@ import numpy as np
 z2id = {1:0, 6: 1, 7:2, 8:3, 16: 4}
 
 
-def load_qm7(datafile):
+def load_qm7_dataset(datafile):
     mat = si.loadmat(datafile)
     # only use R and Z, and predict T
-    Rs, Zs = mat['R'], mat['Z']
+    Rs, Zs, Ts = mat['R'], mat['Z'], mat['T'].swapaxes(1, 0)
+    # randomly shuffle indices
+    shuffle_idx = np.arange(Rs.shape[0])
+    np.random.shuffle(shuffle_idx)
+    Rs, Zs, Ts = Rs[shuffle_idx], Zs[shuffle_idx], Ts[shuffle_idx] 
     new_Rs, new_Zs, Ds = [], [], []
-    Ts = mat['T']
+    Ts = (Ts - Ts.mean()) / Ts.std()
     
     for i, Z in enumerate(Zs):
         non_zeros = np.nonzero(Z)[0]
@@ -24,5 +28,4 @@ def load_qm7(datafile):
         D = np.linalg.norm(abs_D, axis=-1)
         Ds.append(D)
 
-    return new_Rs, new_Zs, Ds
-
+    return new_Rs, new_Zs, Ds, Ts
