0705b8818fd1a0df47ba0b380916e0bc74ec3205
Zhiming Xu <zhimng.xu@gmail.com>
2021-10-03 10:37:01 -0400

    add physnet

--- nn.py
+++ nn.py
@@ -1,9 +1,11 @@
 import torch as th
+from torch._C import namedtuple_eigenvalues_eigenvectors
 import torch.nn as nn
 import torch.nn.functional as F
 import dgl
 import dgl.nn as dglnn
 import scipy.sparse as sp
+from torch.nn.modules import activation
 
 
 class AtomEmbedding(nn.Module):
@@ -201,3 +203,174 @@ class PropertyPrediction(nn.Module):
         h = th.einsum('nf,f->f', x, self.W) + self.b
         h = self.nn(h)
         return self.loss(h, target)
+
+
+class ResidualLayer(nn.Module):
+    def __init__(self, in_size, out_size, activation_fn, use_bias=True, drop_prob=.5):
+        super(ResidualLayer, self).__init__()
+        self._keep_prob = drop_prob
+        self.activation_fn = activation_fn
+        self.dense1 = nn.Linear(in_size, out_size, bias=use_bias)
+        self.dense2 = nn.Linear(in_size, out_size, bias=use_bias)
+        self.dropout = nn.Dropout(drop_prob)
+
+    def forward(self, x):
+        if self.activation_fn:
+            y = self.dropout(self.activation_fn(x))
+        else:
+            y = self.dropout(x)
+        x = x + self.dense2(self.dense1(y))
+        return x
+
+
+class PhysNetInteractionLayer(nn.Module):
+    def __init__(self, K, F, num_residual, activation_fn=None, drop_prob=.5):
+        super(PhysNetInteractionLayer, self).__init__()
+        self.activation_fn = activation_fn
+        self.dropout = nn.Dropout(drop_prob)
+        self.k2f = nn.Linear(K, F, bias=False)
+        self.dense_i = nn.Linear(F, F)
+        self.dense_j = nn.Linear(F, F)
+        self.residuals = nn.ModuleList()
+        for _ in range(num_residual):
+            self.residuals.append(ResidualLayer(F, F, activation_fn, drop_prob))
+        self.dense = nn.Linear(F, F) 
+        self.u = nn.Parameter(nn.rand(F))
+
+    def forward(self, x, rbf, idx_i, idx_j):
+        if self.activation_fn:
+            xa = self.dropout(self.activation_fn(x))
+        else:
+            xa = self.dropout(x)
+        
+        g = self.k2f(rbf)
+        xi = self.dense_i(xa)
+        xj = th.scatter_add_(0, idx_i, g*th.gather(self.dense_j(xa), 0, idx_j))
+
+        m = xi + xj
+
+        for layer in self.residuals:
+            m = layer(m)
+        
+        if self.activation_fn:
+            m = self.activation_fn(m)
+        
+        x = self.u * x + self.dense(m)
+        return x
+
+
+class PhysNetRBFLayer(nn.Module):
+    def __init__(self, cutoff, K):
+        super(PhysNetRBFLayer, self).__init__()
+        self.K = K
+        self.cutoff = cutoff
+        def softplus_inverse(x):
+            return x + th.log(-th.exp(x)-1)
+        centers = softplus_inverse(th.linspace(1., th.exp(-cutoff), K))
+        self.centers = nn.functional.softplus(centers)
+
+        widths = [softplus_inverse((.5/((1-th.exp(-cutoff))/K))**2)] * K
+        self.widths = nn.functional.softplus(widths)
+
+    def cutoff_fn(self, D):
+        x = D/self.cutoff
+        x3 = x**3
+        x4 = x3*x
+        x5 = x4*x
+        return th.where(x<1, 1 - 6*x5 + 15*x4 - 10*x3, th.zeros_like(x))
+
+    def forward(self, D):
+        D = th.unsqueeze(D, -1)
+        rbf = self.cutoff_fn(D) * th.exp(-self.widths*(th.exp(-D)-self.centers)**2)
+        return rbf
+
+
+class PhysNetInteractionBlock(nn.Module):
+    def __init__(self, K, F, num_residual_atom, num_residual_interaction, activation_fn, drop_prob=.5):
+        super(PhysNetInteractionBlock, self).__init__()
+        self.dropout = nn.Dropout(drop_prob)
+        self.activation_fn = activation_fn
+        self.interaction = PhysNetInteractionLayer(K, F, num_residual_interaction, activation_fn, drop_prob)
+        self.residuals = nn.ModuleList()
+        for _ in range(num_residual_atom):
+            self.residuals.append(ResidualLayer(F, F, activation_fn=activation_fn, drop_prob=drop_prob))
+        
+    def forward(self, x, rbf, idx_i, idx_j):
+        x = self.interaction(x, rbf, idx_i, idx_j)
+        for layer in self.residuals:
+            x = layer(x)
+        return x
+
+
+class PhysNetOutputBlock(nn.Module):
+    def __init__(self, F, num_residual, activation_fn, drop_prob=.5):
+        super(PhysNetOutputBlock, self).__init__()
+        self.activation_fn = activation_fn
+        self.residuals = nn.ModuleList()
+        for _ in range(num_residual):
+            self.residuals.append(ResidualLayer(F, F, activation_fn=activation_fn, drop_prob=drop_prob))
+        self.dense = nn.Linear(F, 2, bias=False)
+
+    def forward(self, x):
+        for layer in self.residuals:
+            x = layer(x)
+        if self.activation_fn:
+            x = self.activation_fn(x)
+        return self.dense(x)
+
+
+def shifted_softplus(x):
+    return nn.functional.softplus(x) - th.log(2.)
+
+
+class PhysNet(nn.Module):
+    def __init__(self, F, K, short_cutoff, long_cutoff=None, num_blocks=3, num_residual_atom=2,
+                 num_residual_interaction=2, num_residual_output=1, drop_prob=.5, activation_fn=shifted_softplus):
+        super(PhysNet, self).__init__()
+        self.num_blocks = num_blocks
+        self.atom_embedding = AtomEmbedding(20, F)
+        self.rbf_layer = PhysNetRBFLayer(short_cutoff, K)
+        self.interaction_blocks = nn.ModuleList()
+        self.output_blocks = nn.ModuleList()
+        for _ in range(num_blocks):
+            self.interaction_blocks.append(
+                PhysNetInteractionBlock(K, F, num_residual_atom, num_residual_interaction,
+                activation_fn, drop_prob)
+            )
+            self.output_blocks.append(
+                PhysNetOutputBlock(F, num_residual_output, activation_fn, drop_prob)
+            )
+
+    def calculate_interatom_distance(self, R, idx_i, idx_j, offsets=None):
+        Ri = th.gather(R, idx_i)
+        Rj = th.gather(R, idx_j)
+        if offsets:
+            R += offsets
+        Dij = th.sqrt(nn.functional.relu(th.sum((Ri-Rj)**2, -1)))
+        return Dij
+
+    def forward(self, Z, R, idx_i, idx_j, offsets=None, short_idx_i=None, short_idx_j=None, short_offsets=None):
+        D_ij_lr = self.calculate_interatom_distance(R, idx_i, idx_j, offsets=offsets)
+        if short_idx_i is not None and short_idx_j is not None:
+            D_ij_short = self.calculate_interatom_distance(R, short_idx_i, short_idx_j, offsets=offsets)
+        else:
+            short_idx_i = idx_i
+            short_idx_j = idx_j
+            D_ij_short = D_ij_lr
+        
+        rbf = self.rbf_layer(D_ij_short)
+        x = th.gather(self.atom_embedding, Z)
+
+        E_total, Q_total = 0, 0
+        nhloss, last_out2 = 0, 0
+        for i in range(self.num_blocks):
+            x = self.interaction_blocks[i](x, rbf, short_idx_i, short_idx_j)
+            out = self.output_blocks[i](x)
+            E_total += out[:, 0]
+            Q_total += out[:, 1]
+            out2 = out**2
+            if i > 0:
+                nhloss += th.mean(out2/(out2 + last_out2 + 1e-7))
+        
+        return E_total, Q_total, nhloss
+            
