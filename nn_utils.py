ce46f52205ad51eef37faf9be7349d71fdc86fa6
Zhiming Xu <zhimng.xu@gmail.com>
2021-10-23 16:04:12 -0400

    delete unused function

--- nn_utils.py
+++ nn_utils.py
@@ -1,4 +1,3 @@
-import torch as th
 from torch import nn
 import math
 import numpy as np
@@ -10,53 +9,3 @@ def shifted_softplus(x):
 
 def softplus_inverse(x):
     return x + np.log(-np.expm1(-x))
-
-
-def quaternion_multiply(a, b):
-    """
-    Quaternion_multiply function from FAIR's pytorch3d package
-    Multiply two quaternions.
-    Usual torch rules for broadcasting apply.
-    Args:
-        a: Quaternions as tensor of shape (..., 4), real part first.
-        b: Quaternions as tensor of shape (..., 4), real part first.
-    Returns:
-        The product of a and b, a tensor of quaternions shape (..., 4).
-    """
-    aw, ax, ay, az = th.unbind(a, -1)
-    bw, bx, by, bz = th.unbind(b, -1)
-    ow = aw * bw - ax * bx - ay * by - az * bz
-    ox = aw * bx + ax * bw + ay * bz - az * by
-    oy = aw * by - ax * bz + ay * bw + az * bx
-    oz = aw * bz + ax * by - ay * bx + az * bw
-    quaternions = th.stack((ow, ox, oy, oz), -1)
-    return th.where(quaternions[..., 0:1]<0, -quaternions, quaternions)
-
-
-def quaternion_to_matrix(quaternions):
-    """
-    Convert rotations given as quaternions to rotation matrices.
-    Args:
-        quaternions: quaternions with real part first,
-            as tensor of shape (..., 4).
-    Returns:
-        Rotation matrices as tensor of shape (..., 3, 3).
-    """
-    r, i, j, k = th.unbind(quaternions, -1)
-    two_s = 2.0 / (quaternions * quaternions).sum(-1)
-
-    o = th.stack(
-        (
-            1 - two_s * (j * j + k * k),
-            two_s * (i * j - k * r),
-            two_s * (i * k + j * r),
-            two_s * (i * j + k * r),
-            1 - two_s * (i * i + k * k),
-            two_s * (j * k - i * r),
-            two_s * (i * k - j * r),
-            two_s * (j * k + i * r),
-            1 - two_s * (i * i + j * j),
-        ),
-        -1,
-    )
-    return o.reshape(quaternions.shape[:-1] + (3, 3))
