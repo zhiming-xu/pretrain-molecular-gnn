43dbda3f2bc964ad98204d9d97bea8756a5e0d1b
Zhiming Xu <zhimng.xu@gmail.com>
2021-11-05 20:28:15 -0400

    add property prediction code

--- train_physnet.py
+++ train_physnet.py
@@ -10,25 +10,24 @@ from datetime import datetime
 from torch_geometric.transforms import Compose, ToDevice
 from torch_geometric.datasets import QM9
 from torch_geometric.loader import DataLoader
-from collections import defaultdict
 
 from data_utils import QM7Dataset, DistanceAndPlanarAngle
-from nn import PhysNetInteractionMsgPassing, PhysNetPretrain, PropertyPrediction
+from nn import PhysNetPretrain, PropertyPrediction
 
 
 parser = ArgumentParser('PhysNet')
-parser.add_argument('-data_dir', type=str, default='data')
+parser.add_argument('-data_dir', type=str, default='~/qm9')
 parser.add_argument('-dataset', type=str, default='qm9')
 parser.add_argument('-pretrain_batch_size', type=int, default=128)
 parser.add_argument('-ckpt_step', type=int, default=1)
 parser.add_argument('-ckpt_file', type=str)
-parser.add_argument('-pretrain_size', type=int, default=30000)
 parser.add_argument('-num_epochs', type=int, default=100)
 parser.add_argument('-lr', type=float, default=1e-3)
 parser.add_argument('-cuda', action='store_true')
 parser.add_argument('-pretrain', action='store_true')
 parser.add_argument('-test', action='store_true')
-parser.add_argument('-predict_train_ratio', type=float, default=.8)
+parser.add_argument('-pred_train_ratio', type=float, default=.8)
+parser.add_argument('-pred_batch_size', type=int, default=128)
 parser.add_argument('-hidden_size', type=int, default=128)
 parser.add_argument('-num_pred_layers', type=int, default=3)
 parser.add_argument('-pred_epochs', type=int, default=200)
@@ -61,7 +60,7 @@ def train(args):
     for epoch in tqdm(range(args.num_epochs)):
         loss_atom_types, loss_bond_types, loss_bond_lengths, loss_bond_angles, \
             loss_length_klds, loss_angle_klds, total_losses = [], [], [], [], [], [], []
-        for sample_idx, data in tqdm(enumerate(dataset), leave=False):
+        for batch_idx, data in enumerate(tqdm(dataset, leave=False)):
             model.zero_grad()
             Z, R, idx_ijk, bonds, bond_type, bond_length, bond_angle = data.z, data.pos, data.idx_ijk, \
                 data.edge_index, data.bond_type, data.bond_length, data.bond_angle
@@ -83,14 +82,15 @@ def train(args):
             loss_length_klds.append(loss_length_kld.detach().cpu().numpy())
             loss_angle_klds.append(loss_angle_kld.detach().cpu().numpy())
             total_losses.append(total_loss.detach().cpu().numpy())
-            sw.add_scalar('Debug/Atom Type', loss_atom_type, sample_idx)
-            sw.add_scalar('Debug/Bond Type', loss_bond_type, sample_idx)
-            sw.add_scalar('Debug/Bond Length', loss_bond_length, sample_idx)
-            sw.add_scalar('Debug/Bond Angle', loss_bond_angle, sample_idx)
-            sw.add_scalar('Debug/Length KLD', loss_length_kld, sample_idx)
-            sw.add_scalar('Debug/Angle KLD', loss_angle_kld, sample_idx)
-            sw.add_scalar('Debug/Total', total_loss, sample_idx)
-
+            # for DEBUG
+            sw.add_scalar('Debug/Atom Type', loss_atom_type, batch_idx)
+            sw.add_scalar('Debug/Bond Type', loss_bond_type, batch_idx)
+            sw.add_scalar('Debug/Bond Length', loss_bond_length, batch_idx)
+            sw.add_scalar('Debug/Bond Angle', loss_bond_angle, batch_idx)
+            sw.add_scalar('Debug/Length KLD', loss_length_kld, batch_idx)
+            sw.add_scalar('Debug/Angle KLD', loss_angle_kld, batch_idx)
+            sw.add_scalar('Debug/Total', total_loss, batch_idx)
+ 
         total = len(dataset)
         sw.add_scalar('Loss/Atom Type', sum(loss_atom_types)/total, epoch)
         sw.add_scalar('Loss/Bond Type', sum(loss_bond_types)/total, epoch)
@@ -111,7 +111,15 @@ def pred(args):
     with open(f'logs/{args.running_id}_predict/args.json', 'w') as f:
         json.dump(vars(args), f)
     data_file = os.path.join(args.data_dir, args.dataset)
-    dataset = QM7Dataset(data_file)
+    if args.dataset == 'qm7':
+        dataset = QM7Dataset(data_file)
+        raise DeprecationWarning('use QM9 instead')
+    elif args.dataset == 'qm9':
+        qm9 = QM9(args.data_dir, transform=Compose(
+            [DistanceAndPlanarAngle(), ToDevice(th.device('cuda') if args.cuda else th.device('cpu'))]
+        ))
+        qm9.data.y = (qm9.data.y-qm9.data.y.mean(dim=0)) / qm9.data.y.std(dim=0)
+        dataset = DataLoader(qm9, batch_size=args.pred_batch_size)
     # dataloader = DataLoader(dataset, args.train_batch_size)
     pretrain_model = PhysNetPretrain()
 
@@ -128,38 +136,37 @@ def pred(args):
     optimizer2 = SGD(pred_model.parameters(), lr=1e-3)
 
     total = len(dataset)
-    num_train = round(total * args.predict_train_ratio)
+    num_train = round(total * args.pred_train_ratio)
     num_test = total - num_train
-    E_mean = dataset.E.mean()
-    E_std = dataset.E.std()
     for epoch in tqdm(range(args.pred_epochs)):
-        pred_losses, test_losses = [], []
+        train_losses, test_losses = [], []
         cnt = 0
         for data in tqdm(dataset, leave=False):
-            Z, R, idx_i, idx_j, E = data['Z'], data['R'], data['idx_i'], data['idx_j'], data['E']
-            E_normalized = (E - E_mean) / E_std
+            Z, R, bonds = data.z, data.pos, data.edge_index
+            molecule_idx = th.cat([th.zeros(data.ptr[i]-data.ptr[i-1])+i-1 for i in range(1,data.ptr.shape[0])])
+            molecule_idx = molecule_idx.long()
+
             if cnt < num_train:
                 pretrain_model.zero_grad()
                 pred_model.zero_grad()
-                if args.cuda:
-                    Z, R, E_normalized = Z.cuda(), R.cuda(), E_normalized.cuda()
-                h = pretrain_model.encode(Z, R, idx_i, idx_j)
-                loss = pred_model(h, E_normalized)
-                loss.backward()
-                pred_losses.append(loss)
-                # optimizer1.step()
+                h = pretrain_model.encode(Z, R, bonds)
+                loss = pred_model(h, molecule_idx, data.y)
+                loss.mean().backward()
+                train_losses.append(loss.detach().cpu().mean(dim=0))
+                optimizer1.step()
                 optimizer2.step()
             else:
                 with th.no_grad():
-                    if args.cuda:
-                        Z, R, E_normalized = Z.cuda(), R.cuda(), E_normalized.cuda()
-                    h = pretrain_model.encode(Z, R, idx_i, idx_j)
-                    loss = pred_model(h, E_normalized)
-                    test_losses.append(loss)
+                    h = pretrain_model.encode(Z, R, bonds)
+                    loss = pred_model(h, molecule_idx, data.y)
+                    test_losses.append(loss.detach().cpu().mean(dim=0))
             cnt += 1
-        
-        sw.add_scalar('Loss/Pred', sum(pred_losses)/num_train, epoch)
-        sw.add_scalar('Loss/Test', sum(test_losses)/num_test, epoch)
+
+        train_losses = th.stack(train_losses)
+        test_losses = th.stack(test_losses)
+        for i in range(train_losses.shape[-1]):
+            sw.add_scalar('Prediction/Train target #%s' % i, train_losses[:,i].mean(), epoch)
+            sw.add_scalar('Prediction/Test target #%s' % i, test_losses[:,i].mean(), epoch)
 
 
 def main():
