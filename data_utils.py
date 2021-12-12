9680861119bac2968909cbc510de2731f633d601
Zhiming <zhimng.xu@gmail.com>
2021-12-12 14:54:26 -0500

    fix data util

--- data_utils.py
+++ data_utils.py
@@ -241,7 +241,7 @@ class PMNetTransform(BaseTransform):
     def __init__(self) -> None:
        super().__init__()
         
-    def __call__(self, data, alpha=0.2):
+    def __call__(self, data, alpha=0.15):
         # calculate bond angle
         (src, dst), pos = data.edge_index, data.pos
         # prepare the (i,j,k) triplet for bond angle
@@ -321,6 +321,7 @@ class PMNetTransform(BaseTransform):
         D = np.diag(np.sum(A, 1))                                           # D^ = Sigma A^_ii
         dinv = fractional_matrix_power(D, -0.5)                             # D^(-1/2)
         A_tilde = np.matmul(np.matmul(dinv, A), dinv)                       # A~ = D^(-1/2) x A^ x D^(-1/2)
-        data.diffusion = th.FloatTensor(alpha * inv((np.eye(A.shape[0]) - (1 - alpha) * A_tilde)))    # a(I_n-(1-a)A~)^-1
+        diffusion = th.FloatTensor(alpha * inv((np.eye(A.shape[0]) - (1 - alpha) * A_tilde)))    # a(I_n-(1-a)A~)^-1
+        data.edge_weight = diffusion[data.edge_index[0], data.edge_index[1]]
 
         return data
