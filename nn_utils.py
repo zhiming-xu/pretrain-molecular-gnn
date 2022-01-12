efa615375fa02d0b5136f4915d70f14832a11b62
Zhiming <zhimng.xu@gmail.com>
2022-01-12 17:44:58 -0500

    add kld weight scheduler

--- nn_utils.py
+++ nn_utils.py
@@ -1,3 +1,4 @@
+from socket import SocketKind
 from torch import nn
 import math
 import numpy as np
@@ -9,3 +10,18 @@ def shifted_softplus(x):
 
 def softplus_inverse(x):
     return x + np.log(-np.expm1(-x))
+
+
+class CyclicKLWeight:
+    def __init__(self, cycle_epoch, high=1.) -> None:
+        self.cycle_epoch = cycle_epoch
+        self.high = high
+        self.iter = 0
+
+    def step(self):
+        self.iter += 1
+        cur = self.iter % self.cycle_epoch
+        if cur <= self.cycle_epoch // 2:
+            return self.high / self.cycle_epoch * 2 * cur
+        else:
+            return self.high
